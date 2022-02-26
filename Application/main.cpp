eb1b2e69ad012741d06b9f3cb64a86ce1930575e
ME-Repo <emad.impm@gmail.com>
2022-02-26 17:22:50 +0200

    Add initial implementation for Executer class.

--- Application/main.cpp
+++ Application/main.cpp
@@ -18,25 +18,63 @@ bool test2()
     return true;
 }
 
+bool test3()
+{
+    std::cout << "TEST-3" << std::endl;   
+    return true;
+}
+
+
 int main()
 {
     std::cout << "\n\n\n\nTEEEEEEEEEEEESSSSSSSSSSSSSSSSSSSSSSST" << std::endl;
 
-    behaviorOutput out1;
-    out1.setValue(test1);
+    bool res = false;
+    bool err = false;
+    bool exists = false;
+   
+    genericInput<std::string> in1("no1"); 
+    behaviorOutput out1(test1);
 
-    behaviorOutput out2(nullptr);
+    genericInput<std::string> in2("no2"); 
+    behaviorOutput out2;
 
-    bool t1 = out1.executeOutput();
-    std::cout << "TEST-1 output = " << t1 << std::endl;   
+    genericInput<std::string> in3("no3"); 
+    behaviorOutput out3(test3);
 
-    bool t2 = out2.executeOutput();
-    std::cout << "TEST-2 output = " << t2 << std::endl;   
+    genericInput<std::string> in4("no4"); 
 
-    out1.clearValue();
+    Executer ex;
 
-    t1 = out1.executeOutput();
-    std::cout << "TEST-1 output = " << t1 << std::endl;   
+    std::pair<const absInput*, const absOutput*> pr1;
+    std::pair<const absInput*, const absOutput*> pr2;
+    std::pair<const absInput*, const absOutput*> pr3;
+
+    pr1.first = &in1; 
+    pr1.second = &out1;
+
+    pr2.first = &in2; 
+    pr2.second = &out2;
+
+    pr3.first = &in3; 
+    pr3.second = &out3;
+
+    err = ex.registerInToOut(pr1, pr2, pr3);
+
+    std::cout << "err = " << (int)err << std::endl;
+
+    res = ex.executeOutput(&in1);
+    res = ex.executeOutput(&in2);
+    res = ex.executeOutput(&in3);
+    std::cout << "res = " << (int)res << std::endl;
+
+    exists = ex.inputExists(&in1);
+    std::cout << "exists = " << (int)exists << std::endl;
+
+    exists = ex.inputExists(&in4);
+    std::cout << "exists = " << (int)exists << std::endl;
+
+
 
     sleep(1);
     return 0;
--- inc/Executer/Executer.hpp
+++ inc/Executer/Executer.hpp
@@ -1,26 +1,51 @@
 
-#ifndef ROSETTA_HEADER_H
-#define ROSETTA_HEADER_H
+#ifndef EXECUTER_HEADER_H
+#define EXECUTER_HEADER_H
 
 #include "absInput.hpp"
 #include "absOutput.hpp"
+#include <utility>
 #include <unordered_map>
+
+typedef std::pair<const absInput*,const absOutput*> customPair;
 
 class Executer
 {
 public:
+    // Constructor
     Executer();
 
+    // Copy Constructor
+    Executer(Executer& exec);
+
+    // Move Constructor
+    Executer(Executer&& exec);
+
+    // Destructor
     ~Executer();
 
-    bool registerInOut(const absInput* inputVariablePtr, const absOutput* outputBehaviorPtr);
+    // Method to register inputs with outputs as pairs
+    template <typename fPair, typename... nPairs>  
+    typename std::enable_if<std::is_convertible<fPair, customPair>::type::value, bool>::type
+    registerInToOut(fPair firstPair, nPairs... nextPairs);
 
-    bool executeOutput(const absInput* inputVariablePtr);
+    // Method to register inputs with outputs individually
+    bool registerInToOut(const absInput* inputPtr, const absOutput* outputPtr);
 
-    bool inputExists(const absInput* inputVariablePtr);
+    // Method to execute the output
+    bool executeOutput(const absInput* inputVariablePtr) const;
+
+    // Method to check wether or not the input exists already in the executer
+    bool inputExists(const absInput* inputVariablePtr) const;
 
 private:
-    std::unordered_map<absInput*, absOutput*> parserMap;
+    // Map which has the mapping between inputs and outputs
+    std::unordered_map<const absInput*, const absOutput*> m_parserMap;
+
+    // Private helper
+    inline bool registerInToOut(){return true;}
 };
 
-#endif  /* ROSETTA_HEADER_H */
+#include "./../../src/Executer/Executer.cpp"
+
+#endif  /* EXECUTER_HEADER_H */
